bd027e3c06073e5950a77cb8aadabd52429f6218
Mani Movassagh <mani.mgh@gmail.com>
2025-04-07 01:04:41 +0200

    Update main function to demonstrate string manipulation and reference usage

--- main.c
+++ main.c
@@ -6,5 +6,9 @@ int main(int argc, char const *argv[])
  */
     printf("Hello, Mani let us play with C!\n");
     printf("Nice language!\n");
-    return 0;
+    char name[] = "Mani";
+    char *nameRef = name;
+    printf("Referenced name: %s\n", nameRef);
+    nameRef[2] = 'o';
+    printf("Updated name: %s\n", name);
 }
